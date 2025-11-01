56a6839a1aa10ec1009d2c951dd9aa045dc83f70
Rakib <spyer.rakib@gmail.com>
2025-11-01 12:24:57 +0600

    Update handleCommandEvent.js

--- includes/handle/handleCommandEvent.js
+++ includes/handle/handleCommandEvent.js
@@ -1,46 +1,48 @@
 module.exports = function ({ api, models, Users, Threads, Currencies }) {
-    const logger = require("../../utils/log.js");
-    return function ({ event }) {
-        const { allowInbox } = global.config;
-        const { userBanned, threadBanned } = global.data;
-        const { commands, eventRegistered } = global.client;
-        var { senderID, threadID } = event;
-        senderID = String(senderID);
-        threadID = String(threadID);
-        if (userBanned.has(senderID) || threadBanned.has(threadID) || (allowInbox == false && senderID == threadID)) return;
-        for (const eventReg of eventRegistered) {
-            const cmd = commands.get(eventReg);
-            var getText2;
+  const logger = require("../../utils/log.js");
 
-            if (cmd.languages && typeof cmd.languages == 'object') {
-                getText2 = (...values) => {
-                    const commandModule = cmd.languages || {};
-                    if (!commandModule.hasOwnProperty(global.config.language)) 
-                        return api.sendMessage(global.getText('handleCommand','notFoundLanguage', cmd.config.name), threadID, messengeID); 
-                    var lang = cmd.languages[global.config.language][values[0]] || '';
-                    for (var i = values.length - 1; i >= 0; i--) {
-                        const expReg = RegExp('%' + (i + 1), 'g');
-                        lang = lang.replace(expReg, values[i]);
-                    }
-                    return lang;
-                };
-            } else {
-                getText2 = () => {};
-            }
+  return async function ({ event }) {
+    const { allowInbox } = global.config;
+    const { userBanned, threadBanned } = global.data;
+    const { commands, eventRegistered } = global.client;
 
-            try {
-                const Obj = {};
-                Obj.event = event;
-                Obj.api = api;
-                Obj.models = models;
-                Obj.Users = Users;
-                Obj.Threads = Threads;
-                Obj.Currencies = Currencies;
-                Obj.getText = getText2;
-                if (cmd) cmd.handleEvent(Obj);
-            } catch (error) {
-                logger(global.getText('handleCommandEvent', 'moduleError', cmd.config.name), 'error');
-            }
-        }
-    };
+    let { senderID, threadID } = event;
+    senderID = String(senderID);
+    threadID = String(threadID);
+
+    // নিষিদ্ধ user/thread স্কিপ করো
+    if (userBanned.has(senderID) || threadBanned.has(threadID) || (allowInbox === false && senderID === threadID)) return;
+
+    for (const eventReg of eventRegistered) {
+      const cmd = commands.get(eventReg);
+      if (!cmd || typeof cmd.handleEvent !== "function") continue;
+
+      let getText2 = () => '';
+      if (cmd.languages && typeof cmd.languages === 'object') {
+        getText2 = (...values) => {
+          const commandModule = cmd.languages || {};
+          if (!commandModule.hasOwnProperty(global.config.language)) {
+            return api.sendMessage(
+              global.getText('handleCommand', 'notFoundLanguage', cmd.config.name),
+              threadID,
+              event.messageID
+            );
+          }
+          let lang = cmd.languages[global.config.language][values[0]] || '';
+          for (let i = values.length - 1; i >= 0; i--) {
+            const expReg = RegExp('%' + (i + 1), 'g');
+            lang = lang.replace(expReg, values[i]);
+          }
+          return lang;
+        };
+      }
+
+      try {
+        const Obj = { event, api, models, Users, Threads, Currencies, getText: getText2 };
+        await cmd.handleEvent(Obj);
+      } catch (error) {
+        logger(`${global.getText('handleCommandEvent', 'moduleError', cmd?.config?.name || 'Unknown')} | ${error.message}`, 'error');
+      }
+    }
+  };
 };
