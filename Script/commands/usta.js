1cbd51257394fd8d7522fb8c28baa69e25c06937
Rakib <spyer.rakib@gmail.com>
2025-11-11 17:27:51 +0600

    Update usta.js

--- Script/commands/usta.js
+++ Script/commands/usta.js
@@ -1,7 +1,3 @@
-const axios = require("axios");
-const fs = require("fs");
-const path = require("path");
-
 module.exports.config = {
     name: "usta",
     version: "1.0",
@@ -10,28 +6,21 @@ module.exports.config = {
     description: "ম্যানশন বা রিপ্লাই করা ইউজারকে GIF এর মাধ্যমে আক্রমণ",
     commandCategory: "Fun",
     usages: "[reply/tag]",
-    cooldowns: 5,
-    dependencies: {}
+    cooldowns: 5
 };
 
 module.exports.run = async function({ api, event }) {
     try {
-        let mention = event.senderID;
-        let tagName = "এই নে উষ্টা খা 🦵";
+        let mention = event.senderID; // ডিফল্ট: মেসেজ পাঠানো ইউজার
+        if (event.type === "message_reply") mention = event.messageReply.senderID; // রিপ্লাই
+        else if (event.mentions && Object.keys(event.mentions).length > 0) mention = Object.keys(event.mentions)[0]; // ম্যানশন
 
-        if (event.type === "message_reply") {
-            mention = event.messageReply.senderID;
-        } else if (event.mentions && Object.keys(event.mentions).length > 0) {
-            mention = Object.keys(event.mentions)[0];
-        }
-
-        const gifPath = __dirname + "/usta.gif";
-
-        api.sendMessage({
-            body: `@${mention} ${tagName}`,
-            mentions: [{ tag: tagName, id: mention }],
-            attachment: fs.createReadStream(gifPath)
+        return api.sendMessage({
+            body: `@${mention} এই নে উষ্টা খা 🦵`,
+            mentions: [{ tag: `@${mention}`, id: mention }],
+            attachment: ["https://raw.githubusercontent.com/bdrakib123/cyber-bot-rakib/main/Script/commands/cache/canvas/usta.gif"]
         }, event.threadID);
+
     } catch (err) {
         console.log(err);
     }
